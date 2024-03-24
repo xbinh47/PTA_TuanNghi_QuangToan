4cef90d900c240f824fd3c6c758fbfb7ea7d2a6e
Ford1611 <lamford1611@gmail.com>
2024-03-24 14:24:06 +0700

    init img folder

--- mainwindow.py
+++ mainwindow.py
@@ -1,24 +1,91 @@
-# This Python file uses the following encoding: utf-8
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QMessageBox
+from PyQt6 import uic
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+class Register(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ui/register.ui", self)
+        self.name = ""
+        self.btnRegister.clicked.connect(self.Register)
+        self.btn_login.clicked.connect(self.showLoginPage)
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_MainWindow
+    def Register(self):
+        self.name = self.txtFullName.text()
+        email = self.txtEmail.text
+        password = self.txtPassword.text()
 
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        super().__init__(parent)
+        if not self.name:
+            msg_box.setText("Vui lòng nhập tên!")
+            msg_box.exec
+            return
+        if not email:
+            msg_box.setText("Vui lòng nhập email!")
+            msg_box.exec
+            return
+        if not password:
+            msg_box.setText("Vui lòng nhập mật khẩu!")
+            msg_box.exec()
+            return
+        if not self.agree.isChecked():
+            msg_box.setText("Vui lòng đọc và đồng ý với các điều khoản của AdoptMate!")
+            msg_box.exec
+            return
+        
+        success_box.setText("Bạn đã đăng kí tài khoản thành công!")
+        self.close()
 
+    def showLoginPage(self):
+        loginPage.show()
+        self.close()
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    widget = MainWindow()
-    widget.show()
-    sys.exit(app.exec())
+class Login(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ui/login.ui",self)
+        self.btn_login.clicked.connect(self.check_login)
+        self.btn_register.clicked.connect(self.show_register)
+
+    def check_login(self):
+        email = self.txtEmail.text()
+        password = self.txtPassword.text()
+
+        if not email:
+            msg_box.setText("Vui lòng nhập lại email!")
+            msg_box.exec
+            return
+    
+        if not password:
+            msg_box.setText("Vui lòng nhập lại mật khẩu!")
+            msg_box.exec()
+            return
+        
+        if email == "admin@gmail.com" and password == "123456":
+            success_box.setText("Đăng nhập thành công!")
+            success_box.exec()
+            return
+        else:
+            msg_box.setText("Email hoặc mật khẩu không đúng!")
+            msg_box.exec()
+            return
+        
+    def show_register(self):
+        registerPage
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+
+    loginPage = Login()
+    loginPage.show()
+    registerPage = Register()
+
+    msg_box = QMessageBox()
+    msg_box.setWindowTitle("Error")
+    msg_box.setIcon(QMessageBox.Icon.Warning)
+
+    success_box = QMessageBox()
+    success_box.setWindowTitle("Success")
+    success_box.setIcon(QMessageBox.Icon.Information)
+
+    app.exec()
